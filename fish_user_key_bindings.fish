4d9edf7513412526a07a08b0b1ebdcc645eef8bb
Jace <jacegem@gmail.com>
2021-12-01 16:29:18 +0900

    Add alias

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -13,6 +13,7 @@ function fish_user_key_bindings
     # PATH
     set PATH $PATH ~/.poetry/bin
     set PATH $PATH ~/.fzf/bin
+    set PATH $PATH ~/.doom.d/bin
 
     # Variables
     set -g theme_display_git no
