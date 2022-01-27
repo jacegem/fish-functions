a4cb8389ea95dcb9c274abba84546d9bcc84e956
Jace <jacegem@gmail.com>
2022-01-27 09:39:40 +0900

    Add flutter path

--- fish_user_key_bindings.fish
+++ fish_user_key_bindings.fish
@@ -13,7 +13,8 @@ function fish_user_key_bindings
     # PATH
     set PATH $PATH ~/.poetry/bin
     set PATH $PATH ~/.fzf/bin
-    set PATH $PATH ~/.doom.d/bin
+    set PATH $PATH ~/.emacs.d/bin
+    set PATH $PATH ~/sdk/flutter/bin
 
     # Variables
     set -g theme_display_git no
